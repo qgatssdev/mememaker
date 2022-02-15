6b9a9c80353b161ead19ddc85c7ad97037a8e89e
qgatssdev <chivoomodu@gmail.com>
2022-02-15 19:37:50 +0100

    creating resuable styles and randomizing index

--- src/Meme/Meme.js
+++ src/Meme/Meme.js
@@ -1,14 +1,41 @@
 import React, { useEffect, useState } from 'react';
+import styles from './styles.module.css';
 
 export const Meme = () => {
   const [memes, setMemes] = useState([]);
+  const [memeIndex, setMemeIndex] = useState(0);
+
+  const shuffleMemes = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * i);
+      const temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+  };
   useEffect(() => {
     fetch('https://api.imgflip.com/get_memes').then((res) =>
       res.json().then((res) => {
-        const memes = res.data.memes;
-        setMemes(memes);
+        const meme = res.data.memes;
+        shuffleMemes(meme);
+        setMemes(meme);
       })
     );
   }, []);
-  return <div>Meme</div>;
+  return memes.length ? (
+    <div className={styles.container}>
+      <button onClick={() => console.log(0)} className={styles.generate}>
+        Generate
+      </button>
+      <button
+        onClick={() => setMemeIndex(memeIndex + 1)}
+        className={styles.skip}
+      >
+        Skip
+      </button>
+      <img src={memes[memeIndex].url} alt="" />
+    </div>
+  ) : (
+    <></>
+  );
 };
