a5cedb8b4a3579ca35cebe19bff210e75c57ca52
qgatssdev <chivoomodu@gmail.com>
2022-04-10 23:44:24 +0100

    meme generated

--- src/MemeGenerated/MemeGenerated.js
+++ src/MemeGenerated/MemeGenerated.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const MemeGenerated = () => {
-  // const [memes, setMemes] = useState([]);
+  // const [memes, setMemes] = useState([]);z
   return (
     <div>
       <h1>Hi</h1>
