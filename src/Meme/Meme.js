93b7f79c9be1830b6206aa4129701b437a676af4
qgatssdev <chivoomodu@gmail.com>
2022-05-02 13:38:27 +0100

    hengen

--- src/Meme/Meme.js
+++ src/Meme/Meme.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import styles from './styles.module.css';
 import { useNavigate } from 'react-router-dom';
 
-// Component: Memecards jkas a meme card
+// Component: Memecards 
 export const Meme = () => {
   const [memes, setMemes] = useState([]);
   const [memeIndex, setMemeIndex] = useState(0);
